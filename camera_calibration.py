4d94fe9a26fbb97458c1730ef7e4bcc96e734351
William <william.stucchi@mail.polimi.it>
2023-03-07 21:27:51 +0100

    new chessboard scacco.it

--- camera_calibration.py
+++ camera_calibration.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2 as cv
 import glob
 
-chessboardSize = (8,5)
+chessboardSize = (7, 7)
 frameSize = (2560, 1440)
 
 # termination criteria
@@ -10,13 +10,13 @@ criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((chessboardSize[0] * chessboardSize[1], 3), np.float32)
-objp[:,:2] = np.mgrid[0:chessboardSize[0],0:chessboardSize[1]].T.reshape(-1,2)
+objp[:, :2] = np.mgrid[0:chessboardSize[0], 0:chessboardSize[1]].T.reshape(-1, 2)
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
-images = glob.glob('chessboard_imgs/*.jpg')
+images = glob.glob('scacco.it/*.jpg')
 for fname in images:
     print(fname)
     img = cv.imread(fname)
@@ -28,15 +28,15 @@ for fname in images:
     # If found, add object points, image points (after refining them)
     if ret == True:
         objpoints.append(objp)
-        corners2 = cv.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
+        corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
         imgpoints.append(corners)
         # Draw and display the corners
         cv.drawChessboardCorners(img, chessboardSize, corners2, ret)
-        scale=50
+        scale = 50
         height = int(img.shape[0] * scale / 100)
-        width=int(img.shape[1]*scale/100)
-        dim=(width,height)
-        resized=cv.resize(img,dim,interpolation=cv.INTER_AREA)
+        width = int(img.shape[1] * scale / 100)
+        dim = (width, height)
+        resized = cv.resize(img, dim, interpolation=cv.INTER_AREA)
         cv.imshow('img', resized)
         cv.waitKey(1000)
 
@@ -50,15 +50,15 @@ print("\nDistortion Parameters: \n", dist)
 print("\nRotation Vectors: \n", rvecs)
 print("\nTranslation Vectors: \n", tvecs)
 
-img = cv.imread('chessboard_imgs/20230307_111004.jpg')
+img = cv.imread('scacco.it/20230307_134211.jpg')
 h,  w = img.shape[:2]
-newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
 
 # undistort
 dst = cv.undistort(img, mtx, dist, None, newcameramtx)
 # crop the image
 x, y, w, h = roi
-dst = dst[y:y+h, x:x+w]
+dst = dst[y: y + h, x: x + w]
 cv.imwrite('calibresult.png', dst)
 
 mean_error = 0
@@ -66,4 +66,4 @@ for i in range(len(objpoints)):
     imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
     error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
     mean_error += error
-print( "total error: {}".format(mean_error/len(objpoints)) )
+print("total error: {}".format(mean_error/len(objpoints)))
