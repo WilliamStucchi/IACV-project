8263764d224ad5f28654273b1982255d583ba7b8
William <william.stucchi@mail.polimi.it>
2023-03-15 08:52:33 +0100

    calibration class

---
+++ CameraCalibration.py
@@ -0,0 +1,113 @@
+import numpy as np
+import cv2
+import glob
+import matplotlib.image as mpimg
+
+class CameraCalibration():
+
+    def __init__(self, image_dir, nx, ny):
+        """ Init CameraCalibration.
+        Parameters:
+            image_dir (str): path to folder contains chessboard images
+            nx (int): width of chessboard (number of squares)
+            ny (int): height of chessboard (number of squares)
+        """
+        fnames = glob.glob("{}/*".format(image_dir))
+        objpoints = []
+        imgpoints = []
+
+        # Coordinates of chessboard's corners in 3D
+        objp = np.zeros((nx * ny, 3), np.float32)
+        objp[:, :2] = np.mgrid[0:nx, 0:ny].T.reshape(-1, 2)
+
+        # Go through all chessboard images
+        for f in fnames:
+            img = mpimg.imread(f)
+
+            # Convert to grayscale image
+            gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+            # Find chessboard corners
+            ret, corners = cv2.findChessboardCorners(img, (nx, ny))
+            if ret:
+                imgpoints.append(corners)
+                objpoints.append(objp)
+
+        shape = (img.shape[1], img.shape[0])
+        ret, self.mtx, self.dist, _, _ = cv2.calibrateCamera(objpoints, imgpoints, shape, None, None)
+        print("fammoca")
+        if not ret:
+            raise Exception("Unable to calibrate camera")
+
+    def undistort(self, img):
+        # Convert to grayscale image
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        return cv2.undistort(img, self.mtx, self.dist, None, self.mtx)
+
+    """
+        chessboardSize = (7, 7)
+        frameSize = (2560, 1440)
+    
+        # termination criteria
+        criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    
+        # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+        objp = np.zeros((chessboardSize[0] * chessboardSize[1], 3), np.float32)
+        objp[:, :2] = np.mgrid[0:chessboardSize[0], 0:chessboardSize[1]].T.reshape(-1, 2)
+    
+        # Arrays to store object points and image points from all the images.
+        objpoints = [] # 3d point in real world space
+        imgpoints = [] # 2d points in image plane.
+    
+        images = glob.glob('scacco.it/*.jpg')
+        for fname in images:
+            print(fname)
+            img = cv.imread(fname)
+            gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    
+            # Find the chess board corners
+            ret, corners = cv.findChessboardCorners(gray, chessboardSize, None)
+    
+            # If found, add object points, image points (after refining them)
+            if ret == True:
+                objpoints.append(objp)
+                corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+                imgpoints.append(corners)
+                # Draw and display the corners
+                cv.drawChessboardCorners(img, chessboardSize, corners2, ret)
+                scale = 50
+                height = int(img.shape[0] * scale / 100)
+                width = int(img.shape[1] * scale / 100)
+                dim = (width, height)
+                resized = cv.resize(img, dim, interpolation=cv.INTER_AREA)
+                cv.imshow('img', resized)
+                cv.waitKey(1000)
+    
+        cv.destroyAllWindows()
+    
+        ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, frameSize, None, None)
+    
+        print("Camera Calibrated: ",  ret)
+        print("\nCamera Matrix:\n", mtx)
+        print("\nDistortion Parameters: \n", dist)
+        print("\nRotation Vectors: \n", rvecs)
+        print("\nTranslation Vectors: \n", tvecs)
+    
+        img = cv.imread('scacco.it/20230307_134211.jpg')
+        h,  w = img.shape[:2]
+        newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+    
+        # undistort
+        dst = cv.undistort(img, mtx, dist, None, newcameramtx)
+        # crop the image
+        x, y, w, h = roi
+        dst = dst[y: y + h, x: x + w]
+        cv.imwrite('calibresult.png', dst)
+    
+        mean_error = 0
+        for i in range(len(objpoints)):
+            imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+            error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
+            mean_error += error
+        print("total error: {}".format(mean_error/len(objpoints)))
+    """
--- main.py
+++ main.py
@@ -1,16 +1,60 @@
-# This is a sample Python script.
+import cv2
+from CameraCalibration import CameraCalibration
 
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# initialization
+calibration = CameraCalibration("scacco.it", 7, 7)
+
+print("END")
+
+# read video from file
+cap = cv2.VideoCapture('../video//20230303_145434.mp4')
+
+video_fps = cap.get(cv2.CAP_PROP_FPS),
+total_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+print(f"Frame Per second: {video_fps } \nTotal Frames: {total_frames} \n Height: {height} \nWidth: {width}")
+
+new_width = width / 2
+new_height = height / 2
+
+if (cap.isOpened() == False):
+    print("Error opening video file")
+
+# Read until video is completed
+while (cap.isOpened()):
+
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    if ret == True:
+        # Display the resulting frame
+        img = cv2.resize(frame, (1280, 720))
+
+        # all here
+        temp = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+        # temp = cv2.cvtColor(img, cv2.COLOR_BGR2LUV)
+        # temp = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+        L, A, B = cv2.split(temp)
+        # cv2.imshow('Frame', temp)
+        cv2.imshow('L_Channel', L)
+        cv2.imshow('A_Channel', A)
+        cv2.imshow('B_Channel', B)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        # Press Q on keyboard to exit
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Break the loop
+    else:
+        break
+
+# When everything done, release
+# the video capture object
+cap.release()
+
+# Closes all the frames
+cv2.destroyAllWindows()
